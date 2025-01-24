045a8e7f44e57c9885467ab296971f42a70f710d
Neel Patel <neelp@metropolia.fi>
2025-01-24 11:26:02 +0200

    - Components added

---
+++ YoutubeViewers/YoutubeViewers.WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace YoutubeViewers.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ YoutubeViewers/YoutubeViewers.WPF/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace YoutubeViewers.WPF
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
+    }
+}
